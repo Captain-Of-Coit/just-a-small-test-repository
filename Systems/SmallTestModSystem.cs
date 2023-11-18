240e3fb973fe5a970659c669c5f2efb995a46189
Captain-Of-Coit <admin@captains-haven.org>
2023-11-18 03:38:54 +0100

    rename

--- Systems/SmallTestModSystem.cs
+++ Systems/SmallTestModSystem.cs
@@ -1,4 +1,4 @@
-using Game;
+﻿using Game;
 using Game.Audio;
 using Game.Prefabs;
 using Game.Simulation;
@@ -7,9 +7,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Scripting;
 
-namespace MyCoolMod.Systems
+namespace SmallTestMod.Systems
 {
-    public class MyCoolModSystem : GameSystemBase
+    public class SmallTestModSystem : GameSystemBase
     {
         // private SimulationSystem simulation;
 
